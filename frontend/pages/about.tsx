3c019e6a9cf95d625978c9d870b6addaf53994b0
Tomosius <pecukevicius@gmail.com>
2025-07-09 20:17:00 +0100

    1. React templating and app setup reverted back to app folder 2. Created Initial Layout for Top NavigationLinks.ts

--- frontend/pages/about.tsx
+++
@@ -1,7 +0,0 @@
-export default function Home() {
-    return (
-        <div>
-            <h1>testing if Webstorm submits to same repo</h1>
-        </div>
-    )
-}
--- frontend/pages/index.tsx
+++
@@ -1,8 +0,0 @@
-export default function Home() {
-  return (
-    <div>
-      <h1>Hello from static export!</h1>
-      <p>Tomosius testas balamutas his page will work with next export.</p>
-    </div>
-  )
-}
