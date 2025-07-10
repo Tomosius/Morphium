d09e2682c95fdcffb0ed668af8a29e42ba1da8b7
Tomosius <pecukevicius@gmail.com>
2025-07-10 18:29:02 +0100

    Side Bar code added comments

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -3,6 +3,8 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
     output: 'export',
+      images: { unoptimized: true }, // disables Next.js default image optimization
+
 };
 
 export default nextConfig;
--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,11 +1,13 @@
 export default function Home() {
-  return (
-    <div>
-      <main>
+    return (
+        <div>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. At, aut consectetur doloribus dolorum enim
+                    esse eveniet ex expedita natus placeat quam quibusdam rem repellendus sequi, sint sit ut vero
+                    voluptates!</p>
 
 
-      </main>
 
-    </div>
-  );
+
+        </div>
+    );
 }
