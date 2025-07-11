b612eac8f6b03c6d6759b695c74080aef9a95763
Tomosius <pecukevicius@gmail.com>
2025-07-11 16:45:23 +0100

    main.py
    added code to enable app runing in dev mode

--- main.py
+++ main.py
@@ -1,12 +1,21 @@
 import os
+import sys
 import webview
 
 def main():
-    index_path = os.path.join(
-        os.path.dirname(__file__),
-        'frontend', 'out', 'index.html'
-    )
-    url = f'file://{index_path}'
+    dev_mode = '--dev' in sys.argv
+
+    if dev_mode:
+        url = "http://localhost:3000"
+        print("Running in DEV mode (React live server)...")
+    else:
+        index_path = os.path.join(
+            os.path.dirname(__file__),
+            'frontend', 'out', 'index.html'
+        )
+        url = f'file://{index_path}'
+        print("Running in PRODUCTION mode (built static files)...")
+
     webview.create_window('My App', url)
     webview.start()
 
